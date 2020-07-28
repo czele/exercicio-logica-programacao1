471f1482bf898c80618dea43efb9291404000c38
Camila Zelenovsky <camila_zele@hotmail.com>
2020-07-27 23:42:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meses = 30;
+            Console.WriteLine("Quantos meses você deseja somar?");
+            int quantidade = Convert.ToInt32(Console.ReadLine());
+            
+
+            int total = meses * quantidade;
+
+            Console.WriteLine($"O total de dias é {total}.");
+
+        }
+    }
+}
+;
